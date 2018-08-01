14dde9aa504e2f195770ee1ca3bcd37c1bb9ddfe
Roberto Previdi <hariseldon78@gmail.com>
2018-08-02 01:25:09 +0200

    first commit

---
+++ server/routes.ts
@@ -0,0 +1,8 @@
+import {Application} from 'express';
+import examplesRouter from './api/controllers/examples/router';
+import L from './common/logger'
+
+export default function routes(app: Application): void {
+	app.use('/api/v1/examples', examplesRouter);
+
+};
